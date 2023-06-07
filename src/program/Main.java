e4738f5eafccb82849bdf9a4d24bee6034f1803c
Alexandre Santos <Alexandre_MFS@outlook.com>
2023-06-07 19:28:09 -0300

    Department entity class

---
+++ src/program/Main.java
@@ -0,0 +1,9 @@
+package program;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department obj = new Department(1,"books");
+    }
+}
