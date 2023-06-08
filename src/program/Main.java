bda825b11a9fc350f4e5cfbaa74fecea1295f5f1
Alexandre Santos <Alexandre_MFS@outlook.com>
2023-06-08 01:45:08 -0300

    Seller entity class

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,9 +1,15 @@
 package program;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1,"books");
+        Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.00,obj);
+
+        System.out.println(seller);
     }
 }
