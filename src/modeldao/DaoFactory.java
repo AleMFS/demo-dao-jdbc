c26b4d71ed640e1f47fb321b22384f3250eedc66
Alexandre Santos <Alexandre_MFS@outlook.com>
2023-06-09 10:57:06 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/modeldao/DaoFactory.java
@@ -0,0 +1,9 @@
+package modeldao;
+
+import modeldaoimpl.SellerDaoJDBC;
+
+public class DaoFactory {
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+}
--- src/program/Main.java
+++ src/program/Main.java
@@ -2,6 +2,8 @@ package program;
 
 import model.entities.Department;
 import model.entities.Seller;
+import modeldao.DaoFactory;
+import modeldao.SellerDao;
 
 import java.util.Date;
 
@@ -9,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1,"books");
         Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.00,obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
